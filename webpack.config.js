915589a38eaaf19fbefae0e8de9858e3b590978b
Michael Cook <entozoon@gmail.com>
2019-08-06 19:30:53 +0100

    Quick SCSS compilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,48 @@
-"use strict";
 const webpack = require("webpack"),
   HtmlWebpackPlugin = require("html-webpack-plugin"),
   { CleanWebpackPlugin } = require("clean-webpack-plugin"),
   CopyWebpackPlugin = require("copy-webpack-plugin"),
+  ExtractTextPlugin = require(`extract-text-webpack-plugin`),
   path = require("path");
-module.exports = {
-  entry: "./src/main.js",
-  resolve: {
-    extensions: [".js"]
-  },
-  devtool: "inline-source-map",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-    globalObject: "this"
-  },
-  devServer: {
-    contentBase: "./build"
-  },
-  plugins: [
-    // new CleanWebpackPlugin([path.resolve(__dirname, "build")]),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([{ from: "static" }]),
-    new HtmlWebpackPlugin({
-      template: "static/index.html"
-    })
-  ],
-  performance: { hints: false }
+module.exports = (env, argv) => {
+  const extractCss = new ExtractTextPlugin({
+    filename: `app.css`
+  });
+
+  return {
+    entry: {
+      main: [`${__dirname}/src/main.js`, `${__dirname}/src/app.scss`]
+    },
+    resolve: {
+      modules: [`node_modules`, `src`],
+      extensions: [".js", ".scss"]
+    },
+    devtool: "inline-source-map",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+      globalObject: "this"
+    },
+    devServer: {
+      contentBase: "./build"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          // use: extractCss.extract({
+          use: ["style-loader", "css-loader", "sass-loader"]
+        }
+      ]
+    },
+    plugins: [
+      // new CleanWebpackPlugin([path.resolve(__dirname, "build")]),
+      new CleanWebpackPlugin(),
+      new CopyWebpackPlugin([{ from: "static" }]),
+      new HtmlWebpackPlugin({
+        template: "static/index.html"
+      })
+    ],
+    performance: { hints: false }
+  };
 };

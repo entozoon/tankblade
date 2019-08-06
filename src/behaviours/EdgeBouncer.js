d83769d7847582e43ea92b0d0c61eb0795120ae0
Michael Cook <entozoon@gmail.com>
2019-08-06 13:49:47 +0100

    Better boinking!

--- src/behaviours/EdgeBouncer.js
+++ src/behaviours/EdgeBouncer.js
@@ -3,32 +3,40 @@ import { constrain } from "../lib/utilities";
 export default class {
   constructor() {
     this.fuckingUp = this.fuckingUp;
+    this.fuckingUpReset = this.fuckingUpReset;
     this.bounceUpdate = this.bounceUpdate;
-    this.fuckupTimer;
+    this.fuckingUpTimer;
+    this.inset = 6;
   }
-  fuckingUp() {
-    clearTimeout(this.fuckupTimer);
-    this.fuckupTimer = setTimeout(() => {
-      // Won't necessarily do anything, if it boinked it's way back in
+  fuckingUp(dt) {
+    this.fuckingUpTimer += dt;
+
+    // Won't necessarily do anything, like if it boinked it's way back in
+    if (this.fuckingUpTimer > 2000) {
       this.setPosition({
-        y: constrain(this.position.y, 0, 64),
-        x: constrain(this.position.x, 0, 64)
+        y: constrain(this.position.y, this.inset, 64 - this.inset),
+        x: constrain(this.position.x, this.inset, 64 - this.inset)
       });
-    }, 2000);
+      this.setThrust({ y: 0, x: 0 });
+    }
+  }
+  fuckingUpReset() {
+    this.fuckingUpTimer = 0;
   }
   bounceUpdate(dt) {
     const nudge = this.thrustPower * 2,
       offsetY = 3,
       offsetX = 6;
     if (this.position.y < offsetY || this.position.y > 64 - offsetY) {
-      this.fuckingUp();
       this.thrust.y =
         -Math.sign(this.thrust.y) * (Math.abs(this.thrust.y) + nudge);
-    }
-    if (this.position.x < offsetX || this.position.x > 64 - offsetX) {
-      this.fuckingUp();
+      this.fuckingUp(dt);
+    } else if (this.position.x < offsetX || this.position.x > 64 - offsetX) {
       this.thrust.x =
         -Math.sign(this.thrust.x) * (Math.abs(this.thrust.x) + nudge);
+      this.fuckingUp(dt);
+    } else {
+      this.fuckingUpReset();
     }
   }
 }

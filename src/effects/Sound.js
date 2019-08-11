f26d47676dc3b54b824868700e3cfdb610fbb2dc
Myke <entozoon@gmail.com>
2019-08-11 20:37:36 +0100

    Tidying

--- src/effects/Sound.js
+++ src/effects/Sound.js
@@ -4,7 +4,9 @@ import Pixi from "../engines/Pixi";
 const soundResources = {
   intro: "intro.mp3",
   bgm: "bgm.mp3",
-  bounce: "bounce.mp3",
+  bounce1: "bounce1.mp3",
+  bounce2: "bounce2.mp3",
+  bounce3: "bounce3.mp3",
   hairy: "hairy.mp3"
 };
 
